7867336c2df222ad5e76f4c7a4897c5cc99b8f77
xMelaa <melaka.mk@gmail.com>
2023-11-05 13:51:58 +0100

    score

--- Screens/games/quiz/quiz.tsx
+++ Screens/games/quiz/quiz.tsx
@@ -11,44 +11,69 @@ type RootStackParamList = {
   choose: undefined;
   memory: undefined;
   quizresult: undefined;
+  games: undefined;
 };
 
 type HomeScreenProps = {
-  navigation: NativeStackNavigationProp<RootStackParamList, "Second">; // Upewnij się, że to jest zgodne z Twoją konfiguracją nawigatora
+  navigation: NativeStackNavigationProp<RootStackParamList, "Second">;
 };
 
 export default function QuizGame({ navigation }: HomeScreenProps) {
-    const [currentQuestion, setCurrentQuestion] = useState(0);
-   // const getQuiz = async () => {};
-    useEffect(() => {
-        //getQuiz();
-       // console.log(quizData[0].options[2].src) //test quizdata
-    }, [])
-    
-    const handleAnswer = (selectedAnswer: any) => {
-      const answer = quizData[currentQuestion]?.answer
-      if(answer === selectedAnswer.name){
-        alert("Prawidłowa odpowiedź!")
-      }else{
-        alert("Zła odpowiedź. Spróbuj ponownie.")
-      }
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  // // const getQuiz = async () => {};
+  // useEffect(() => {
+  //   //getQuiz();
+  //   // console.log(quizData[0].options[2].src) //test quizdata
+  // }, []);
+
+  const handleAnswer = (selectedAnswer: any) => {
+    const answer = quizData[currentQuestion]?.answer;
+    if (answer === selectedAnswer.name) {
+      setScore((prevScore) => prevScore + 1);
+      alert("Prawidłowa odpowiedź!");
+    } else {
+      alert("Zła odpowiedź. Spróbuj ponownie.");
     }
-  
-    return (
+
+    const nextQ = currentQuestion + 1;
+    if (nextQ < quizData.length) {
+      setCurrentQuestion(nextQ);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  return (
     <View style={styles.container}>
       <Text>Quiz</Text>
-      <View style={styles.question}>
-        <Text>{quizData[currentQuestion]?.question}</Text>
-      </View>
-      <View style={styles.answers}>
-        {quizData[currentQuestion]?.options.map((item) => {
-          return <TouchableOpacity onPress={()=>handleAnswer(item)} style={styles.answer}>
-          <Text>{item.name}</Text>
-          <Image style={styles.answerImage} source={item.src} />
-        </TouchableOpacity>
-        })}
-       
-      </View>
+      {showScore ? (
+        <View>
+          <Text>{score}</Text>
+          <TouchableOpacity>
+          <Button title="Powrót" onPress={() => navigation.push("games")} />
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <>
+          <View style={styles.question}>
+            <Text>{quizData[currentQuestion]?.question}</Text>
+          </View>
+          <View style={styles.answers}>
+            {quizData[currentQuestion]?.options.map((item) => {
+              return (
+                <TouchableOpacity
+                  onPress={() => handleAnswer(item)}
+                  style={styles.answer}>
+                  <Text>{item.name}</Text>
+                  <Image style={styles.answerImage} source={item.src} />
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        </>
+      )}
       <View style={styles.buttons}>
         <TouchableOpacity>
           <Text>WRÓĆ</Text>
@@ -73,26 +98,25 @@ const styles = StyleSheet.create({
     height: "100%",
   },
   question: {
-    marginVertical: 20,    
+    marginVertical: 20,
   },
   answers: {
-    marginVertical: 20,    
+    marginVertical: 20,
     //height: "100%",
     width: "75%",
-    gap: 30, 
+    gap: 30,
     display: "flex",
     flexDirection: "row",
     flexWrap: "wrap",
     justifyContent: "space-between",
-  
   },
-  answer:{    
+  answer: {
     width: "20%",
     position: "relative",
     aspectRatio: 1,
   },
-  answerImage:{ 
-    aspectRatio: 1/1
+  answerImage: {
+    aspectRatio: 1 / 1,
   },
   buttons: {
     marginBottom: 16,
