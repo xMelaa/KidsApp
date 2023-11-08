4bdbf04d69b50d1336f4e0058456883bac77102f
xMelaa <melaka.mk@gmail.com>
2023-11-08 12:16:05 +0100

    grid

--- Screens/words/animals.tsx
+++ Screens/words/animals.tsx
@@ -1,32 +1,48 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  Image,
+  FlatList
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { Animals} from "../../data/data";
+import { Animals } from "../../data/data";
 
 type RootStackParamList = {
-   Second: undefined;
-    choose: undefined;
-    words: undefined;
-    games: undefined;
-    animal: {animalName: string};
-  };
+  Second: undefined;
+  choose: undefined;
+  words: undefined;
+  games: undefined;
+  animal: { animalName: string };
+};
 
 type HomeScreenProps = {
-    navigation: NativeStackNavigationProp<RootStackParamList, "Second">; // Upewnij się, że to jest zgodne z Twoją konfiguracją nawigatora
-  };
+  navigation: NativeStackNavigationProp<RootStackParamList, "Second">; // Upewnij się, że to jest zgodne z Twoją konfiguracją nawigatora
+};
 
 export default function AnimalsScreen({ navigation }: HomeScreenProps) {
   const animalNames = Object.keys(Animals);
   return (
     <View style={styles.container}>
       <Text>Zwierzęta</Text>
-      {animalNames.map((animalName) => (
-        <Button
-          key={animalName}
-          title={animalName}
-          onPress={() => navigation.push("animal", { animalName: animalName })}
-        />
-      ))}
+      <View style={styles.buttonsContainer}>
+        {animalNames.map((animalName) => (
+          <>
+            <TouchableOpacity
+              key={animalName}
+              onPress={() =>
+                navigation.push("animal", { animalName: animalName })
+              }>
+              <Image
+                source={Animals[animalName].photo}
+                style={{ width: 200, height: 200 }}></Image>
+            </TouchableOpacity>
+          </>
+        ))}
+      </View>
     </View>
   );
 }
@@ -37,11 +53,17 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
- 
+    width: "100%",
+    height: "100%"
   },
-  buttonsContainer:{
+  buttonsContainer: {
+    width: "85%",
+    display: "flex",
     flexDirection: "row",
-    alignItems: "center",
-    gap: 50
-  }
+    flexWrap: "wrap",
+    alignItems: "center", 
+    gap: 50,
+    height: "100%",
+    marginVertical: 20
+  },
 });
