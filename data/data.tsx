1090a8d376204ec77af6a2b8cd80e54ebcfb5ede
xMelaa <melaka.mk@gmail.com>
2023-11-10 11:32:23 +0100

    data update

--- data/data.tsx
+++ data/data.tsx
@@ -512,15 +512,870 @@ export let Animals: AnimalsType = {
   },
 };
 
-export let Vehicles = {
+type VehicleType = {
+  name: string;
+  photo: any;
+  sound: any;
+  ciekawostki: string[];
+};
+
+type VehiclesType = {
+  [key: string]: VehicleType;
+};
+
+export let Vehicles: VehiclesType = {
   Car: {
     name: "Samochód",
-    photo: require("../img/example.jpg"),
+    photo: require("../img/Pojazdy/Samochód.png"),
     sound: "brum brum",
-    ciekawostki: [],
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Ambulans: {
+    name: "Ambulans",
+    photo: require("../img/Pojazdy/Ambulans.jpg"),
+    sound: "brum brum",
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Autobus: {
+    name: "Autobus",
+    photo: require("../img/Pojazdy/Autobus.png"),
+    sound: "brum brum",
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Balon: {
+    name: "Balon",
+    photo: require("../img/Pojazdy/Balon.jpg"),
+    sound: "brum brum",
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Ciężarówka: {
+    name: "Ciężarówka",
+    photo: require("../img/Pojazdy/Ciężarówka.jpg"),
+    sound: "brum brum",
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Helikopter: {
+    name: "Helokopter",
+    photo: require("../img/Pojazdy/Helikopter.jpg"),
+    sound: "brum brum",
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Hulajnoga: {
+    name: "Hulajnoga",
+    photo: require("../img/Pojazdy/Hulajnoga.png"),
+    sound: "brum brum",
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Koparka: {
+    name: "Koparka",
+    photo: require("../img/Pojazdy/Koparka.jpg"),
+    sound: "brum brum",
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Łódka: {
+    name: "Łódka",
+    photo: require("../img/Pojazdy/Łódka.jpg"),
+    sound: "brum brum",
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Motocykl: {
+    name: "Motocykl",
+    photo: require("../img/Pojazdy/Motocykl.jpg"),
+    sound: "brum brum",
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Pociąg: {
+    name: "Pociąg",
+    photo: require("../img/Pojazdy/Pociąg.jpg"),
+    sound: "brum brum",
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Policja: {
+    name: "Policja",
+    photo: require("../img/Pojazdy/Policja.jpg"),
+    sound: "brum brum",
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Rower: {
+    name: "Rower",
+    photo: require("../img/Pojazdy/Rower.png"),
+    sound: "brum brum",
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Samolot: {
+    name: "Samolot",
+    photo: require("../img/Pojazdy/Samolot.png"),
+    sound: "brum brum",
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Statek: {
+    name: "Statek",
+    photo: require("../img/Pojazdy/Statek.png"),
+    sound: "brum brum",
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Straż: {
+    name: "Straż",
+    photo: require("../img/Pojazdy/Straż.png"),
+    sound: "brum brum",
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Traktor: {
+    name: "Traktor",
+    photo: require("../img/Pojazdy/Traktor.png"),
+    sound: "brum brum",
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Tramwaj: {
+    name: "Tramwaj",
+    photo: require("../img/Pojazdy/Tramwaj.png"),
+    sound: "brum brum",
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
   },
 };
 
-export let Fruits = {};
+type FruitType = {
+  name: string;
+  photo: any;
+  ciekawostki: string[];
+};
 
-export let Vegetables = {};
+type FruitsType = {
+  [key: string]: FruitType;
+};
+
+export let Fruits: FruitsType = {
+  Agrest: {
+    name: "Agrest",
+    photo: require("../img/Owoce/Agrest.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Ananas: {
+    name: "Ananas",
+    photo: require("../img/Owoce/Ananas.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Jabłko: {
+    name: "Jabłko",
+    photo: require("../img/Owoce/apple.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Arbuz: {
+    name: "Arbuz",
+    photo: require("../img/Owoce/Arbuz.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Awokado: {
+    name: "Agrest",
+    photo: require("../img/Owoce/Awokado.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Banan: {
+    name: "Banan",
+    photo: require("../img/Owoce/Banan.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Borówka: {
+    name: "Borówka",
+    photo: require("../img/Owoce/blueberry.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Brzoskwinia: {
+    name: "Brzoskwinia",
+    photo: require("../img/Owoce/Brzoskwinia.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Cytryna: {
+    name: "Cytryna",
+    photo: require("../img/Owoce/Cytryna.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Daktyle: {
+    name: "Daktyle",
+    photo: require("../img/Owoce/Daktyle.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Figa: {
+    name: "Figa",
+    photo: require("../img/Owoce/Figa.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Grejpfrut: {
+    name: "Grejpfrut",
+    photo: require("../img/Owoce/Grejpfrut.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Jagody: {
+    name: "Jagody",
+    photo: require("../img/Owoce/Jagody.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Jeżyna: {
+    name: "Jeżyna",
+    photo: require("../img/Owoce/Jeżyna.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Kaki: {
+    name: "Kaki",
+    photo: require("../img/Owoce/Kaki.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Kiwi: {
+    name: "Kiwi",
+    photo: require("../img/Owoce/Kiwi.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Liczi: {
+    name: "Liczi",
+    photo: require("../img/Owoce/Liczi.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Limonka: {
+    name: "Limonka",
+    photo: require("../img/Owoce/Limonka.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Malina: {
+    name: "Malina",
+    photo: require("../img/Owoce/Maliny.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Mandarynka: {
+    name: "Mandarynka",
+    photo: require("../img/Owoce/Mandarynka.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Mango: {
+    name: "Mango",
+    photo: require("../img/Owoce/Mango.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Morela: {
+    name: "Morela",
+    photo: require("../img/Owoce/Morela.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Nektarynka: {
+    name: "Nektarynka",
+    photo: require("../img/Owoce/Nektarynka.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Oliwki: {
+    name: "Oliwki",
+    photo: require("../img/Owoce/Oliwki.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Gruszka: {
+    name: "Gruszka",
+    photo: require("../img/Owoce/pear.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Pigwa: {
+    name: "Pigwa",
+    photo: require("../img/Owoce/Pigwa.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Pomarańcza: {
+    name: "Pomarańcza",
+    photo: require("../img/Owoce/Pomarańcza.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Granat: {
+    name: "Granat",
+    photo: require("../img/Owoce/pomegranade.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Porzeczka: {
+    name: "Porzeczka",
+    photo: require("../img/Owoce/Porzeczka.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Poziomka: {
+    name: "Poziomka",
+    photo: require("../img/Owoce/Poziomka.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Śliwka: {
+    name: "Śliwka",
+    photo: require("../img/Owoce/Śliwka.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Truskawka: {
+    name: "Truskawka",
+    photo: require("../img/Owoce/Truskawka.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Winogrono: {
+    name: "Winogrono",
+    photo: require("../img/Owoce/Winogrono.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Wiśnia: {
+    name: "Wiśnia",
+    photo: require("../img/Owoce/Wiśnia.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Żurawina: {
+    name: "Żurawina",
+    photo: require("../img/Owoce/Żurawina.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+};
+
+type VegetableType = {
+  name: string;
+  photo: any;
+  ciekawostki: string[];
+};
+
+type VegetablesType = {
+  [key: string]: VegetableType;
+};
+
+export let Vegetables: VegetablesType = {
+  Bakłażan: {
+    name: "Bakłażan",
+    photo: require("../img/Warzywa/Bakłażan.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Bób: {
+    name: "Bób",
+    photo: require("../img/Warzywa/Bób.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Brokuł: {
+    name: "Brokuł",
+    photo: require("../img/Warzywa/Brokuł.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Burak: {
+    name: "Burak",
+    photo: require("../img/Warzywa/Burak.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Cebula: {
+    name: "Cebula",
+    photo: require("../img/Warzywa/Cebula.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Ciecierzyca: {
+    name: "Ciecierzyca",
+    photo: require("../img/Warzywa/Ciecierzyca.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Cukinia: {
+    name: "Cukinia",
+    photo: require("../img/Warzywa/Cukinia.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Czosnek: {
+    name: "Czosnek",
+    photo: require("../img/Warzywa/Czosnek.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Dynia: {
+    name: "Dynia",
+    photo: require("../img/Warzywa/Dynia.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Fasola: {
+    name: "Fasola",
+    photo: require("../img/Warzywa/Fasola.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Groszek: {
+    name: "Groszek",
+    photo: require("../img/Warzywa/Groszek.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Imbir: {
+    name: "Imbir",
+    photo: require("../img/Warzywa/Imbir.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Jarmuż: {
+    name: "Jarmuż",
+    photo: require("../img/Warzywa/Jarmuż.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Kalafior: {
+    name: "Kalafior",
+    photo: require("../img/Warzywa/Kalafior.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Kapusta: {
+    name: "Kapusta",
+    photo: require("../img/Warzywa/Kapusta.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Kukurydza: {
+    name: "Kukurydza",
+    photo: require("../img/Warzywa/Kukurydza.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Marchewka: {
+    name: "Marchewka",
+    photo: require("../img/Warzywa/Marchewka.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Ogórek: {
+    name: "Ogórek",
+    photo: require("../img/Warzywa/Ogórkek.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Papryka: {
+    name: "Papryka",
+    photo: require("../img/Warzywa/Papryka.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Pietruszka: {
+    name: "Pietruszka",
+    photo: require("../img/Warzywa/Pietruszka.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Pomidor: {
+    name: "Pomidor",
+    photo: require("../img/Warzywa/Pomidor.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Rzodkiewka: {
+    name: "Rzodkiewka",
+    photo: require("../img/Warzywa/Rzodkiewka1.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Sałata: {
+    name: "Sałata",
+    photo: require("../img/Warzywa/Sałata.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Seler: {
+    name: "Seler",
+    photo: require("../img/Warzywa/Seler.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Szczypiorek: {
+    name: "Szczypiorek",
+    photo: require("../img/Warzywa/Szczypiorek.png"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Szparagi: {
+    name: "Szparagi",
+    photo: require("../img/Warzywa/Szparagi.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Szpinak: {
+    name: "Szpinak",
+    photo: require("../img/Warzywa/Szpinak.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+  Ziemniaki: {
+    name: "Ziemniaki",
+    photo: require("../img/Warzywa/Ziemniaki1.jpg"),
+    ciekawostki: [
+      "ciekawostka 1",
+      "ciekawostka 2",
+      "ciekawostka 3",
+      "ciekawostka 4",
+    ],
+  },
+};
